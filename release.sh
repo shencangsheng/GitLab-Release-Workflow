c62be2f52f4bf8d589a7cf770b57eb9d0ba354c7
jayden <liushiyu>
2021-07-21 10:25:46 +0800

    fix: 补全release脚本

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git tag > tags.txt
+
+before_tag=`cat tags.txt | grep -B 1 $CI_COMMIT_TAG | head -n 1`
+
+bash /changelog.sh $before_tag
+
 sed s/$/"\\\n"/ CHANGELOG.txt|tr -d '\n' > release-notes.txt
 
 description=`cat release-notes.txt`
